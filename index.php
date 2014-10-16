36b889fa4989097b8a4494de655c439c883cfea7
merilinm <merilin.metsamaa@khk.ee>
2014-10-16 09:56:02 +0300

    Add sample calculations

--- index.php
+++ index.php
@@ -7,8 +7,11 @@
 
 <body>
     <h1>See dokument on minu esimene katsetus PHP-ga</h1>
-    <?php echo "tootab"; ?>
-    <br>
-    <?php echo "Unos" . " Dos."; ?>
+    <?php echo "tootab"; ?><br>
+    <?php echo "Unos" . " Dos."; ?><br>
+    <?php echo 5.99 + 6; ?><br>
+    <?php echo 99.1 - 6; ?><br>
+    <?php echo 5 * 6; ?><br>
+    <?php echo 24 / 6; ?><br>
 </body>
 </html>
