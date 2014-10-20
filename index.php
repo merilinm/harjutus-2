d6e9e6e4362d60cee3abd8bf20bf40a2f91ee6ea
merilinm <merilin.metsamaa@khk.ee>
2014-10-20 13:36:07 +0300

    Add sample variables

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 </head>
 
 <body>
-
     <h1>See dokument on minu esimene katsetus PHP-ga</h1>
     <?php // My first string. ?>
     <?php echo "tootab"; ?><br>
@@ -23,5 +22,17 @@
     <?php echo 99.1 - 6; ?><br>
     <?php echo 5 * 6; ?><br>
     <?php echo 24 / 6; ?><br>
+    <?php echo "tootab"; ?><br>
+    <?php echo "Unos" . " Dos."; ?><br>
+    <?php
+    $var1 = 10;
+    echo $var1;
+    echo "<br>";
+    $var2 = 99;
+    echo $var2;
+    echo "<br>";
+    $var3 = "see on s6na";
+    echo $var3;
+?>
 </body>
 </html>
